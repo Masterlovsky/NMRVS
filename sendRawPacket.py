157aa1dcd57078be7fb745175cf7e8657f219e1b
Masterlovsky <masterlovsky@gmail.com>
2021-10-13 21:09:10 +0800

    sendRawPacket.py中 1. 修复了中介域bgp向user发送注册注销响应报文发不出去的bug(主要修复了原ipv6地址字段和payload长度大小端问题) 2. cleanup中增加释放irs_local_ring队列 3. 解析失败时重新加入队列优化为直接执行正确解析函数，减少一次队列操作 TODO：解析暂时发不出去包

--- sendRawPacket.py
+++ sendRawPacket.py
@@ -1,5 +1,3 @@
-import sys
-import socket
 from scapy.all import *
 from scapy.layers.inet6 import IPv6
 from scapy.layers.l2 import Ether
@@ -28,23 +26,90 @@ class IDPNRS(Packet):
                    ]
 
 
-def main():
-    payload_register = "6f" + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" + "66666666666666666666666666666666"
-    payload_deregister = "73" + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" + "66666666666666666666666666666666"
+def packet_creator(command: str, eid="bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"):
+    payload_register = "6f" + eid + \
+        ipv6ToHexString(USER_NA)
+    payload_deregister = "73" + eid + \
+        ipv6ToHexString(USER_NA)
+    payload_bgp_register = "6f" + eid + ipv6ToHexString(USER_NA) + \
+        ipv6ToHexString(CONTROLLER_NA) + "00000001" + \
+        ipv6ToHexString(BGP_NA)
+    payload_bgp_deregister = "73" + eid + ipv6ToHexString(USER_NA) + \
+        ipv6ToHexString(CONTROLLER_NA) + "00000001" + \
+        ipv6ToHexString(BGP_NA)
+
+    # ! 模拟客户端给 controller 发包
     pkt_r = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201", dst="::") / IDP(
         destEID=int("0" * 40, 16)) / IDPNRS(queryType="register") / bytes.fromhex(payload_register)
     pkt_d = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201", dst="::") / IDP(
         destEID=int("0" * 40, 16)) / IDPNRS(queryType="register") / bytes.fromhex(payload_deregister)
     pkt_eq = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201",
                                                    dst="2400:dd01:1037:9:192:168:9:201") / IDP(
-        destEID=int("b" * 40, 16)) / IDPNRS(queryType="resolve")
+        destEID=int(eid, 16)) / IDPNRS(queryType="resolve")
+
+    # ! 模拟controller给bgp发包
+    pkt_bgp_eq = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src=USER_NA,
+                                                       dst=BGP_NA) / IDP(
+        destEID=int(eid, 16)) / IDPNRS(queryType="resolve", source="format1", na=int("0" * 40, 16))
+
+    pkt_bgp_r = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src=USER_NA, dst="::") / IDP(
+        destEID=int("0" * 40, 16)) / IDPNRS(queryType="register") / bytes.fromhex(payload_bgp_register)
+
+    pkt_bgp_d = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src=USER_NA, dst="::") / IDP(
+        destEID=int("0" * 40, 16)) / IDPNRS(queryType="deregister") / bytes.fromhex(payload_bgp_deregister)
+
+    pkt_bgp_eq_wrong = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src=USER_NA,
+                                                             dst=BGP_NA) / IDP(
+        destEID=int(eid, 16)) / IDPNRS(queryType="resolve_w", source="format1", na=int(ipv6ToHexString(CONTROLLER_NA), 16))
+    ret_pkt = None
+    if command == "r":
+        ret_pkt = pkt_r
+    elif command == "eq":
+        ret_pkt = pkt_eq
+    elif command == "d":
+        ret_pkt = pkt_d
+    elif command == "br":
+        ret_pkt = pkt_bgp_r
+    elif command == "beq":
+        ret_pkt = pkt_bgp_eq
+    elif command == "bd":
+        ret_pkt = pkt_bgp_d
+    elif command == "beqw":
+        ret_pkt = pkt_bgp_eq_wrong
+    else:
+        ret_pkt = None
+    ret_pkt.show()
+    hexdump(ret_pkt)
+    return ret_pkt
+
+
+def ipv6ToHexString(ipv6addr: str) -> str:
+    maohao = ipv6addr.count(":")
+    ip6_complete = ipv6addr
+    if maohao < 7:
+        index = ipv6addr.index("::")
+        ip6_complete = ipv6addr[0:index] + ":" * (7-maohao) + ipv6addr[index:]
+    ip_list = ip6_complete.split(":")
+    hexipstr = ""
+    for i in ip_list:
+        hexipstr = hexipstr + "0" * (4-len(i)) + i
+    return hexipstr
+
+
+def main():
+
     # pkt_r = Ether() / IPv6(dst="2400:dd01:1037:201:192:168:47:198")
-    pkt = pkt_eq
-    pkt.show()
-    hexdump(pkt)
+    # pkt = [packet_creator("br"), packet_creator("br", "a"*40), packet_creator("br", "c"*40), packet_creator(
+    #     "beq"), packet_creator("bd"), packet_creator("beq")]
+    pkt = packet_creator("beq", "a" * 40)
+    # pkt = packet_creator("br", "a"*40)
     # sendp(pkt, iface="em4", loop=1, inter=0.2)
-    # sendp(pkt, iface="em4", count=1)
+    sendp(pkt, iface="p7p4", count=1)
+    # sendp(Ether(dst="00:00:00:01:02:03")/IPv6(dst="2400:dd01:1037:100:20::22")/UDP(dport=89), iface="p7p4", count=1)
 
 
 if __name__ == '__main__':
+    USER_NA = "6666:" * 7 + "6666"
+    CONTROLLER_NA = "2400:dd01:1037:100:224::226"
+    BGP_NA = "2400:dd01:1037:100:20::22"
     main()
